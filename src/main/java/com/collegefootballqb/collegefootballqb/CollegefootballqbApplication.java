8d72793d6d1edf7db90e7ae9245e019a7bd81c31
Ivo Araujo <ivo.souza-23@outlook.com>
2025-09-24 17:36:44 -0300

    StartProject

---
+++ src/main/java/com/collegefootballqb/collegefootballqb/CollegefootballqbApplication.java
@@ -0,0 +1,13 @@
+package com.collegefootballqb.collegefootballqb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegefootballqbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegefootballqbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collegefootballqb/collegefootballqb/CollegefootballqbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegefootballqb.collegefootballqb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegefootballqbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
